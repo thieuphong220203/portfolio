0844746ac0d00aa7361350dc558876e1ea82a748
Thieu Phong <tangthieuphong888@gmail.com>
2024-11-21 14:43:19 +0700

    dating profile1

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -33,22 +33,21 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            {/* I am a senior student at <b>Ton Duc Thang</b> University, specializing in web development. With a solid foundation in backend technologies, database design, SOLID principles, and algorithms, I am currently looking for an internship opportunity to further develop my skills and gain real-world experience. */ }
-            I am a senior student at <b>Ton Duc Thang</b> University, specializing in web development.
+            I am a senior student at <b>Ton Duc Thang</b> University, specializing in web development. With a solid foundation in backend technologies, database design, SOLID principles, and algorithms, I am currently looking for an internship opportunity to further develop my skills and gain real-world experience.
           </p>
           <div>
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Like Me?
+              Hire Me
             </Link>
             <Link
               href="./cv/cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Swipe right!!
+                Download CV
               </span>
             </Link>
           </div>
