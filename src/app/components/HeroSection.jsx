269384e216474db4e24da55f4e2a1700d9f63d6e
Thieu Phong <tangthieuphong888@gmail.com>
2024-11-21 14:15:23 +0700

    dating profile

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,14 +40,14 @@ const HeroSection = () => {
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Like Me?
             </Link>
             <Link
               href="./cv/cv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                Swipe right!!
               </span>
             </Link>
           </div>
